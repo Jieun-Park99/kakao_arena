d969c0326483fc29b89e0ade58e7d05b8f2f9bd3
jiunny <jiunny0921@naver.com>
2020-07-24 12:43:01 +0900

    파일합치기 v2

--- Module_Data.py
+++ Module_Data.py
@@ -9,6 +9,7 @@
 import multiprocessing
 threads = multiprocessing.cpu_count()-1
 
+from multiprocessing import Pool
 
 # In[2]:
 
@@ -30,7 +31,7 @@ from itertools import chain
 
 # In[3]:
 
-
+# 데이터 불러오기
 train_data = pd.read_json('train.json', encoding='UTF-8', typ = 'frame')
 valid_data = pd.read_json('val.json', encoding='UTF-8', typ = 'frame')
 song_meta = pd.read_json('song_meta.json', encoding='UTF-8', typ = 'frame')
@@ -42,7 +43,7 @@ genre_gn_all = pd.DataFrame(genre_gn_all, columns = ['gnr_name']).reset_index().
 
 # In[4]:
 
-
+# song_meta에 한국어 장르명도 넣기
 def fn_as_kor_genre(j):
     b=[]
     for i in song_meta.song_gn_gnr_basket[j]:
@@ -60,7 +61,7 @@ def fn_as_kor_genre(j):
 
 # In[8]:
 
-
+# train에 song 장르 채워넣기
 def fn_fill_kor_genre(j):
     uni_genre=[list(chain((list(chain(*song_meta[song_meta['id']==i].song_gn_gnr_basket_kor))))) for i in train_data.songs[j]]
     b = list(set(list(chain(*uni_genre))))
@@ -70,7 +71,7 @@ def fn_fill_kor_genre(j):
         
     return b
 
-
+# 불용어 제거
 def fn_clean_str(text):
     pattern = '([ㄱ-ㅎㅏ-ㅣ]+)'  # 한글 자음, 모음 제거
     text = re.sub(pattern=pattern, repl='', string=text)
@@ -85,3 +86,58 @@ def fn_clean_str(text):
     pattern = re.compile('[\U00010000-\U0010ffff]', flags=re.UNICODE) #마지막점검 제거
     text = re.sub(pattern=pattern, repl='',string=text)
     return text
+
+# khaiii 사용자 사전 추가
+def fn_add_khaiidic(words):
+    _home = os.getcwd()
+    _home
+    
+    f = open("khaiii/rsc/src/preanal.manual",'a')
+
+    for i in range(len(words)):
+        data = "{}* \t {}/NNG\n".format(words[i],words[i])
+        f.write(data)
+    f.close()
+    
+    os.chdir(_home+'/khaiii/rsc')
+    os.system('mkdir -p ../build/share/khaiii')
+    
+    _ppath = 'PYTHONPATH=%s/khaiii/src/main/python/ ./bin/compile_preanal.py --rsc-src=./src --rsc-dir=%s/khaiii/build/share/khaiii'
+    os.system(_ppath % (_home,_home))
+    
+    os.chdir(_home)
+    
+    print("#### Finished adding to dictionary ####")
+    
+def fn_pre_khaiii(i,playlist):
+    tmp_list = []
+    
+    if len(playlist) == playlist.count(' '):
+        # 빈칸으로만 되어있는 행
+        pass
+    elif playlist.find(" ")==-1:
+        # 띄어쓰기 없는 행
+        tmp_list.append(playlist)
+    else:
+        for word in api.analyze(playlist):
+            for morph in word.morphs:
+                # 실질형태소 가져오기
+                if morph.tag in ['NNG', 'NNP', 'NP', 'VV', 'VA', 'XR', 'IC', 'MM', 'MAG', 'MAJ','SN','SL']:
+                    tmp_list.append(morph.lex)
+    if i%100==0:
+        print(i)
+    if i%1000==0:
+        time.sleep(10)
+        
+    return tmp_list
+
+def fn_exc_khaiii(args):
+    return fn_pre_khaiii(*args)
+
+def parallel_product(list_a, list_b):
+    # spark given number of processes
+    p = Pool(threads) 
+    # set each matching item into a tuple
+    job_args = [(item_a, list_b[i]) for i, item_a in enumerate(list_a)] 
+    # map to pool
+    return p.map(fn_exc_khaiii, job_args)
